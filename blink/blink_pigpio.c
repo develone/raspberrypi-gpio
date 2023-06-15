445ccacc337a55ad43f084a2f491a0a8f363bbd0
Edward Vidal Jr <develone@sbcglobal.net>
2023-06-14 20:48:22 -0600

    added a delay function

--- blink/blink_pigpio.c
+++ blink/blink_pigpio.c
@@ -2,11 +2,27 @@
 #include <stdio.h>
 #include <pigpio.h>
 const int RedLED = 21;
+
+uint * x,k;
+
 volatile sig_atomic_t signal_received = 0;
+void del(uint * jj) {
+	uint y,z;
+
+	z=*jj;
+	//printf("%d\n",z);
+	for(y=0;y<z;y++) {
+	}
+}
+
+
 void sigint_handler(int signal) {
    signal_received = signal;
 }
+
 int main() {
+	k = 10;
+    x = &k;
    if (gpioInitialise() == PI_INIT_FAILED) {
       printf("ERROR: Failed to initialize the GPIO interface.\n");
       return 1;
@@ -16,9 +32,11 @@ int main() {
    printf("Press CTRL-C to exit.\n");
    while (!signal_received) {
       gpioWrite(RedLED, PI_HIGH);
-      time_sleep(1);
+      //time_sleep(1);
+	  del(x);	
       gpioWrite(RedLED, PI_LOW);
-      time_sleep(1);
+      //time_sleep(1);
+	  del(x);
    }
    gpioSetMode(RedLED, PI_INPUT);
    gpioTerminate();
