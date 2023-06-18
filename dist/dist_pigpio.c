d9eb28587889734e27fa77a56733043da3cf8d06
Edward Vidal Jr <develone@sbcglobal.net>
2023-06-18 06:23:21 -0600

    devel@pi4-ultibo:~ $ rm -f 4ch.csv
    devel@pi4-ultibo:~ $ minicom
    
    devel@pi4-ultibo:~/raspberrypi-gpio/dist $ make
    
    devel@pi4-ultibo:~/raspberrypi-gpio/dist $ sudo ./pigpio
    
    k = 1;		on 2 usec
    k = 100;	57 - 28 = 29 usec
    
    k = 7000;
    158 usec
    
    k = 66000;
    143628 - 143577 = 51 usec
    
    devel@pi4-ultibo:~/raspberrypi-gpio/dist $ make
    make: Circular dist_pigpio <- dist_pigpio dependency dropped.
    gcc -Wall dist_pigpio.c -lpigpio -o pigpio
    devel@pi4-ultibo:~/raspberrypi-gpio/dist $ sudo ./pigpio
    Press CTRL-C to exit.
    
    devel@pi4-ultibo:~/generate-vcd/sample $ rm -f ~/4ch.csv
    devel@pi4-ultibo:~/generate-vcd/sample $ cat 4ch_header.vcd xx > test2.vcd
    devel@pi4-ultibo:~/generate-vcd/sample $ gtkwave test2.vcd

---
+++ dist/dist_pigpio.c
@@ -0,0 +1,47 @@
+#include <signal.h>
+#include <stdio.h>
+#include <pigpio.h>
+const int RedLED = 21;
+
+uint * x,k;
+
+volatile sig_atomic_t signal_received = 0;
+void del(uint * jj) {
+	uint y,z;
+
+	z=*jj;
+	//printf("%d\n",z);
+	for(y=0;y<z;y++) {
+	}
+}
+
+
+void sigint_handler(int signal) {
+   signal_received = signal;
+}
+
+int main() {
+   if (gpioInitialise() == PI_INIT_FAILED) {
+      printf("ERROR: Failed to initialize the GPIO interface.\n");
+      return 1;
+   }
+   gpioSetMode(RedLED, PI_OUTPUT);
+   signal(SIGINT, sigint_handler);
+   printf("Press CTRL-C to exit.\n");
+   while (!signal_received) {
+      gpioWrite(RedLED, PI_HIGH);
+      //time_sleep(1);
+	k = 7000;
+    x = &k;
+	  del(x);	
+      gpioWrite(RedLED, PI_LOW);
+      //time_sleep(1);
+	k = 66000;
+    x = &k;
+	  del(x);
+   }
+   gpioSetMode(RedLED, PI_INPUT);
+   gpioTerminate();
+   printf("\n");
+   return 0;
+}
